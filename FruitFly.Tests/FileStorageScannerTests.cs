db1b235f86f138706cc01d040cd65e32a8cab0ca
Pavel Prchal <prchalp@gmail.com>
2021-10-09 09:01:34 +0200

    devops changes

---
+++ FruitFly.Tests/FileStorageScannerTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using fruitfly.core;
+
+namespace FruitFly.Tests
+{
+    [TestFixture]
+    public class FileStorageScannerTests
+    {
+        [Test]
+        public void SmokeTest(string content)
+        {
+            // var storage = new FileStorage() as IStorage;
+        }
+
+    }
+}
