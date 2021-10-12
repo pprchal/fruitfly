b7eed7da62a0a61a422145935545f19342466293
Pavel Prchal <prchalp@gmail.com>
2021-10-12 20:06:44 +0200

    test fixing

---
+++ FruitFly.Tests/TileTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using fruitfly.core;
+using fruitfly.objects;
+
+namespace FruitFly.Tests
+{
+    [TestFixture]
+    public class TileTests
+    {
+        [Test]
+        public void T(string content)
+        {
+            var storage = new FileStorage(new fruitfly.Console()) as IStorage;
+            var result = new Post(
+                null, 
+                storage
+            ).Render(null, Constants.MORPH_TILE);
+        }
+
+    }
+}
